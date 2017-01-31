013475bc335ef3ffe25ec1c0c67a692f1d6b4b45
jeongmoon417 <jm5836@naver.com>
2017-01-31 14:33:58 +0900

    Add files via upload

---
+++ redneuronalsimple.py
@@ -0,0 +1,27 @@
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+import tensorflow as tf
+
+x = tf.placeholder("float", [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+
+matm = tf.matmul(x, W)
+y = tf.nn.sotfmax(tf.matmul(x, W) + b)
+y_ = tf.placeholder("float", [None, 10])
+
+cross_entropy = -tf.reduce_sum(_y*tf.log(y))
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+
+sess = tf.Session()
+sess.run(tf.initialize_all_variables())
+
+for i in range(1000):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={x: vatch_xs, y_:batch_ys})
+
+#¸ðµ¨ Æò°¡
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+print sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.lables})
